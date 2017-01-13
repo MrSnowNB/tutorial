f85d077567e881e26ccee6de25e6efa9393b52f1
Mr Snow <mark_snow@nbpsnj.net>
2017-01-13 17:03:35 +0000

    asyncio

--- LYN/syncio/sync.js
+++ LYN/syncio/sync.js
@@ -2,4 +2,6 @@
  
  var newLineFile = fs.readFileSync(process.argv[2],'utf8');//utf8 ensures we get a string avoiding the need for toString
  
- console.log(newLineFile.match(/\n/g).length);
+ console.log(newLineFile.match(/\n/g).length);//match returns an array with each instance of th specified (regEx)
+ 
+ 
